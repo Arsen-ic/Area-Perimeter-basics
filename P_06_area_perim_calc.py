3c8e8e65fdeb75ff3d1734c9b03bc50751533ca2
Arsen-ic <julianr4258@masseyhigh.school.nz>
2025-11-06 10:13:14 +1300

    Area/perimeter calculator

---
+++ P_06_area_perim_calc.py
@@ -0,0 +1,9 @@
+width = float(input("Mayhaps could I have the width sir or ma'am: "))
+height = float(input("Mayhaps could I have the height as well sir or ma'am: "))
+
+area = width * height
+perimeter = 2 * (width + height)
+
+print()
+print(f"I do so believe that the perimeter is {perimeter} units")
+print(f"I do so believe that the area is {area} square units")
